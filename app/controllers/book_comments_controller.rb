55eed205f1de4be610bdc9c831f610d46e472d10
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-07-01 13:46:30 +0000

    moving&changing file-name/content

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,7 +4,6 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-
     respond_to do |format|
       if comment.save
         format.html { redirect_to request.referer, flash.now[:notice] = 'コメントを投稿しました' }
@@ -17,7 +16,6 @@ class BookCommentsController < ApplicationController
     refroute = Rails.application.routes.recognize_path(request.referrer)
     @book = Book.find(refroute[:id])
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-
     respond_to do |format|
       if book_comment.destroy
         format.html { redirect_to request.referer, flash.now[:alert] = '投稿を削除しました' }
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,9 +2,9 @@ class FavoritesController < ApplicationController
   before_action :book_params
 
   def create
+    @book = Book.find(params[:book_id])
     Favorite.create(user_id: current_user.id, book_id: params[:id])
-
-    respond_to do |format|
+     respond_to do |format|
       if favorite.save
         format.html { redirect_to request.referer}
         format.js { render 'favorites/create'}
@@ -13,15 +13,15 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     Favorite.find_by(user_id: current_user.id, book_id: params[:id]).destroy
-
     respond_to do |format|
-      format.html { redirect_to request.refere}
+      format.html { redirect_to request.referer}
       format.js { render 'favorites/destroy'}
     end
   end
 
-  private
+private
 
   def book_params
     @book = Book.find(params[:id])
